89d30547a78a555c75a7053a6938b9b1d04cc16a
Andrew-Zhang <andrewzhang63017@gmailcom>
2020-02-14 21:25:24 -0600

    floor-plan
    in src

---
+++ src/app/floor-plan/floor-plan.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'floor-plan',
+  templateUrl: './floor-plan.component.html',
+  styleUrls: ['./floor-plan.component.css']
+})
+export class FloorPlanComponent {
+}
